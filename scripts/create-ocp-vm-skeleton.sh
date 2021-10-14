a3e6f09771a90b535f32b39971288402cab51d29
Bart van den Heuvel <bvandenh@redhat.com>
2021-10-14 18:07:21 +0200

    added more scripts

--- scripts/create-ocp-vm-skeleton.sh
+++ scripts/create-ocp-vm-skeleton.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 VIRT_DIR=/local/virt
 
-VIRT_DOMAIN='zokahn.local'
+VIRT_DOMAIN='simpletest.nl'
 
 
-nodes="controller1 controller2 controller3"
+nodes="master1 master2 master3"
 NUM=1
 for node in $nodes; do
     echo "Kicking $node into gear"
     virsh destroy $node
     virsh undefine $node
-    virt-install --name=$node --ram=24576 --vcpus=2 \
-                --disk path=$VIRT_DIR/$node.dsk,size=100,bus=virtio \
+    virt-install --name=$node --ram=16384 --vcpus=2 \
+                --disk path=$VIRT_DIR/$node.dsk,size=70,bus=virtio \
                 --pxe --noautoconsole --graphics=vnc --hvm \
                 --network bridge=br1_100,model=virtio,mac=52:54:00:"$NUM"5:bd:2f \
                 --network bridge=br1_100,model=virtio \
@@ -19,14 +19,14 @@ for node in $nodes; do
     NUM=$((NUM+1))
 done
 
-nodes="compute1 compute2"
+nodes="worker1 worker2"
 NUM=4
 for node in $nodes; do
     echo "Kicking $node into gear"
     virsh destroy $node
     virsh undefine $node
-    virt-install --name=$node --ram=4096 --vcpus=2 \
-                --disk path=$VIRT_DIR/$node.dsk,size=100,bus=virtio \
+    virt-install --name=$node --ram=32768 --vcpus=2 \
+                --disk path=$VIRT_DIR/$node.dsk,size=70,bus=virtio \
                 --pxe --noautoconsole --graphics=vnc --hvm \
                 --network bridge=br1_100,model=virtio,mac=52:54:00:"$NUM"5:bd:2f \
                 --network bridge=br1_100,model=virtio \
